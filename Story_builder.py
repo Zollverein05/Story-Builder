2cb8f516c8f2ed8bafcdca59df8be91c380b946a
Zollverein05 <akshitgupta20005@gmail.com>
2024-08-03 16:43:53 +0530

    first commit

---
+++ Story_builder.py
@@ -0,0 +1,16 @@
+# Questions for the user to answer
+noun = input("Choose a noun: ")
+plural_noun = input("Choose a plural noun: ")
+noun2 = input("Choose a noun: ")
+place = input("Name a place: ")
+adjective = input("Choose an adjective (Describing word): ")
+noun3 = input("Choose a noun: ")
+# Print a story from the user input
+print("------------------------------------------")
+print("Be kind to your", noun, '- footed', plural_noun)
+print('For a duck may be somebody\'s', noun2, ',')
+print('Be kind to your', plural_noun, 'in', place)
+print('Where the weather is always', adjective, '. \n')
+print('You may think that is this the', noun3, ',')
+print('Well it is.')
+print('------------------------------------------')
